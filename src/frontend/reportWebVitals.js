d0f7bbba33e2a918b04a80c9182ed1cc50cdc05e
wrangel <11014911+wrangel@users.noreply.github.com>
2026-02-28 17:56:48 +0100

    Bugfix/security (#81)
    * Add error handling to vitals
    
    * Change webvitals to v3+ compatible
    
    * Regress webvitals
    
    * Update and wsp and nginx
    
    * Fixed default.conf comment
    
    * Loosen grip on google on nginx
    
    * Add scanner friendly version
    
    * revert nginx config to abstract altitudes
    
    * removed header from nginx config
    
    * Working base version
    
    * Alter nginx/nginx.conf
    
    * altered default.conf

--- src/frontend/reportWebVitals.js
+++ src/frontend/reportWebVitals.js
@@ -1,12 +1,25 @@
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    import("web-vitals")
+      .then((webVitals) => {
+        // v3+: use onCLS, onFCP, etc. directly
+        if (typeof webVitals.onCLS === "function") {
+          webVitals.onCLS(onPerfEntry);
+          webVitals.onFCP(onPerfEntry);
+          webVitals.onLCP(onPerfEntry);
+          webVitals.onTTFB(onPerfEntry);
+          // onFID deprecated, replaced by onINP in v3
+          if (webVitals.onINP) webVitals.onINP(onPerfEntry);
+        } else {
+          console.warn(
+            "Unexpected web-vitals structure:",
+            Object.keys(webVitals),
+          );
+        }
+      })
+      .catch((err) => {
+        console.error("Failed to load web-vitals:", err);
+      });
   }
 };
 

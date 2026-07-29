1cd5127e689931760b2d9b9579b43ddefb5bcfb4
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2026-07-29 14:01:08 +0200

    Added dummy change

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -9,6 +9,7 @@ import { WebGLManagerProvider } from "./utils/WebGLManager";
 const Home = lazy(() => import("./pages/Home"));
 const Grid = lazy(() => import("./pages/Grid"));
 
+// test
 function App() {
   return (
     <WebGLManagerProvider>
